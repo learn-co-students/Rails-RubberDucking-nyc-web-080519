00627fa713ab949ee87ed7b9e696dfd3f8b4d41c
github username <github email address>
2019-09-03 14:00:23 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,51 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to student_path(@student.id)
+    else
+      flash[:messages] = @student.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    temp_student = Student.new(student_params)
+    if temp_student.valid?
+      @student.update(student_params)
+      redirect_to student_path(@student.id)
+    else
+      flash.now[:messages] = temp_student.errors.full_messages
+      render :edit
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.delete
+    redirect_to students_path
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
+puts "deleting..."
+
+Duck.delete_all
+Student.delete_all
+
+puts "seeding..."
+
 10.times do
   Student.create(name: Faker::Name.name, mod: rand(1..5))
 end
 
+puts "made students. creating ducks..."
+
 5.times do
-  Duck.create(name: Faker::Dog.meme_phrase, description: Faker::Dog.breed)
+  duck = Duck.create(name: Faker::Name.name, description: Faker::Name.name, student_id: rand(1..10))
+  # duck.update(student_id: sample(1..10))
 end
+
+puts "done"
